e50549f57a277c0a554db4fd52da3fd8736e13fe
pravinbhagwat <pravinbhagwat150@gmail.com>
2023-06-17 23:13:07 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,15 @@ const App = () => {
   let tablist = [
     {
       title: "Tab 1",
-      content: "This is content for Tab 1."
+      content: "This is the content for Tab 1."
     },
     {
       title: "Tab 2",
-      content: "This is content for Tab 2."
+      content: "This is the content for Tab 2."
     },
     {
       title: "Tab 3",
-      content: "This is content for Tab 3."
+      content: "This is the content for Tab 3."
     }
   ];
   return (
